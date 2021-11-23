41bc8dfcf36eecc2e709ea9a3b98a785597cf00b
Isabel Morales Sirgo <imoralessirgo@outlook.com>
2021-11-23 18:58:25 -0500

    login lambda

--- js/index.js
+++ js/index.js
@@ -25,8 +25,26 @@ async function registerUser(e) {
 }
 
 function login(e) {
-    e.preventDefault();
-    alert("Feature coming soon");
+    var xhr = new XMLHttpRequest();
+    var username = document.getElementById("username").value;
+    var password = stringToHash(document.getElementById("password").value).toString();
+    var temp;
+    xhr.open("POST", "https://rhoplou1ei.execute-api.us-east-2.amazonaws.com/iteration1/user/login", true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify({
+        username: username,
+        password: password
+    }));
+    xhr.onload = function(){
+        temp = JSON.parse(xhr.response);
+        if(temp.httpStatusCode == 200) {
+            storage.username = username;
+            location.href = "home.html";
+        }
+        else {
+            alert(temp.logMsg);
+        }
+    }
 }
 
 function continueAsGuest(e) {
